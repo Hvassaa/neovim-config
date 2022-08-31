060e3917dd189865838040d6ba30fdbb2d49864d
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2022-08-31 23:04:41 +0200

    added 'so %' keymap, cleaned up

--- lua/general.lua
+++ lua/general.lua
@@ -9,7 +9,12 @@ vim.opt.laststatus = 3
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.wo.number = true
-vim.keymap.set("n", "<F8>", ":setlocal spell! spelllang=en_gb<CR>")
-vim.api.nvim_set_hl(0, "signcolumn", { ctermbg=NONE, guibg=NONE })
-vim.api.nvim_set_hl(0, "Pmenu", { ctermbg=NONE, guibg=NONE })
-vim.api.nvim_set_hl(0, "WinSeparator", { ctermbg=NONE, guibg=NONE })
+local keymap_opts = { silent = true }
+vim.keymap.set("n", "<F8>", ":setlocal spell! spelllang=en_gb<CR>", keymap_opts)
+vim.keymap.set("n", "<F4>", ":w<CR>:so %<cr>", keymap_opts)
+vim.keymap.set("i", "(", "()<left>", keymap_opts)
+vim.keymap.set("i", "{", "{}<left>", keymap_opts)
+vim.keymap.set("i", "[", "[]<left>", keymap_opts)
+vim.api.nvim_set_hl(0, "signcolumn", { bg="NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg="NONE" })
+vim.api.nvim_set_hl(0, "WinSeparator", { bg="NONE" })
