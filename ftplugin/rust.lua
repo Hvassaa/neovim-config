cbba8913e5b79ff9b69324af1eb7a13a2a2f7898
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2022-12-31 11:57:25 +0100

    reboot

--- ftplugin/rust.lua
+++
@@ -1,9 +0,0 @@
-local scomp = require("scomp")
-scomp.setup({
-	command = function()
-		return "rustc -o a " .. vim.fn.expand("%") .. " && ./a"
-	end
-})
-
-vim.keymap.set("n", "<F6>", scomp.run)
-vim.keymap.set("n", "<F7>", scomp.reset_data)
--- init.lua
+++ init.lua
@@ -1,175 +1,67 @@
--- || GENERAL VIM CONF || --
+require('rasmus.treesitter')
+require('rasmus.colors')
+require('rasmus.lsp')
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 vim.opt.termguicolors = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.mouse = "a"
-vim.opt.signcolumn = "yes"
-vim.opt.clipboard = "unnamedplus"
 vim.opt.laststatus = 3
-vim.opt.tabstop = 4
+vim.opt.mouse = 'a'
+vim.opt.signcolumn = 'yes'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.list = true
+vim.opt.listchars = 'tab:» ,trail:-,extends:>,precedes:<,nbsp:+'
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.o.backspace = 'indent,eol,start'
+vim.o.hlsearch = false
+vim.o.incsearch = true
 vim.o.completeopt = 'menuone,noselect'
-vim.opt.shiftwidth = 4
+vim.o.breakindent = true
 vim.wo.number = true
-local keymap_opts = { silent = true }
-vim.keymap.set("n", "<F8>", ":setlocal spell! spelllang=en_gb<CR>", keymap_opts)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
--- does not work???
--- vim.api.nvim_set_hl(0, 'SignColumn', { bg="NONE" })
--- vim.api.nvim_set_hl(0, "pmenu", { fg="white", bg="#4d4d4d" })
--- vim.api.nvim_set_hl(0, "pmenusel", { fg="black", bg="#C7B446" })
--- vim.api.nvim_set_hl(0, "WinSeparator", { bg="NONE" })
-vim.cmd("highlight PMenu guifg=white guibg=gray")
-vim.cmd("highlight SignColumn guibg=NONE")
-vim.cmd("highlight PMenuSel guifg=white guibg=#C7B446")
-vim.cmd("highlight WinSeparator guibg=NONE")
-vim.cmd("highlight StatusLine guibg=black guifg=yellow")
+vim.cmd [[set completeopt-=preview]]
 
--- || PLUGINS || --
+vim.keymap.set('n', '<F8>', ':setlocal spell! spelllang=en_gb<CR>') -- spelling!
+
+vim.g.vimtex_complete_enabled = 1
+-- vim.g.tex_flavor = 'latex'
+-- vim.g.vimtex_view_general_viewer = 'evince'
+
 local use = require('packer').use
 require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use 'lervag/vimtex'
-	use 'numToStr/Comment.nvim'
-	use 'L3MON4D3/LuaSnip'
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/nvim-cmp'
-	use 'saadparwaiz1/cmp_luasnip'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-omni' -- works with vimtex!
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	-- use 'mfussenegger/nvim-jdtls'
-	use { 'Hvassaa/sterm.nvim', branch = "testing" }
-	use 'Hvassaa/scomp.nvim'
+  use 'wbthomason/packer.nvim'
+  use 'numToStr/Comment.nvim'
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use 'folke/which-key.nvim'
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+  use 'hrsh7th/cmp-omni' -- works with vimtex!
+  use 'lervag/vimtex'
 end)
 require('Comment').setup()
-
--- || LSP CONF || --
-local on_attach = function(client, bufnr)
-	local function formatx()
-		vim.lsp.buf.format { async = true }
-	end
-
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', "<M-CR>", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>f', formatx, bufopts)
-end
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-local lspconfig = require('lspconfig')
-
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
--- java is setup with nvim-jdtls in ftplugin/java.lua
-local servers = { 'pyright', 'tsserver', 'rust_analyzer', }
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
-end
-
--- special LSP setup for sumneko_lua
-lspconfig.sumneko_lua.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = {
-				version = 'LuaJIT',
-			},
-			diagnostics = {
-				globals = { 'vim' },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-}
-
--- nvim-cmp setup
-local luasnip = require 'luasnip'
-local cmp = require 'cmp'
-
--- why does sumneko_lua complain without nil check?
-if cmp ~= nil then
-	cmp.setup {
-		snippet = {
-			expand = function(args)
-				luasnip.lsp_expand(args.body)
-			end,
-		},
-		sources = cmp.config.sources({
-			{ name = 'nvim_lsp' },
-			{ name = 'luasnip' },
-			{ name = "omni" },
-			{ name = "buffer", keyword_length = 4 },
-			{ name = "path" },
-		}),
-		mapping = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			['<c-y>'] = cmp.mapping.confirm {
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true,
-			},
-		}),
-		view = {
-			entries = "custom",
-		},
-		experimental = {
-			ghost_text = true,
-		},
-	}
-end
-
--- || LaTeX / vimtex settings ||
-vim.g.vimtex_complete_enabled = 1
-vim.g.tex_flavor = 'latex'
-vim.g.vimtex_view_general_viewer = 'evince'
--- set textwidth to 80 for "text" buffers
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-	pattern = { "*.tex", "*.md", "*.txt" },
-	command = "setlocal textwidth=80"
-})
-
--- || TERMINAL CONFIG || --
--- Double esc to get termimnal normal
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { silent = true })
-
--- be able to change window in terminal mode
-vim.keymap.set("t", "<C-w><C-w>", "<C-\\><C-n><C-w><C-w>", { silent = true })
-
--- automatically go into terminal mode (insert)
-vim.api.nvim_create_autocmd({ "ModeChanged" }, {
-	-- dont run when going from terminal mode to normal-terminal mode
-	pattern = { "[^t]:nt" },
-	-- wanted to use [^ct], otherwise you go into insert after entering command, but then :term wont go into insert :-(
-	command = "startinsert"
-})
-
-vim.api.nvim_create_autocmd({ "TermOpen" }, {
-	-- dont run when going from terminal mode to normal-terminal mode
-	pattern = { "*" },
-	-- can this be done in lua/callback?
-	command = "setlocal nonumber norelativenumber"
-})
-
-vim.keymap.set({ "t", "n" }, "<F5>", require("sterm").toggle, { silent = true })
+require('which-key').setup()
