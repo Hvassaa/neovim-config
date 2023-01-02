6b686fe52076eb33393b847d9df326eca61b1257
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2023-01-02 16:53:42 +0100

    more lazy

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-require('rasmus.treesitter')
-require('rasmus.colors')
-require('rasmus.lsp')
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -28,40 +24,7 @@ vim.cmd [[set completeopt-=preview]]
 
 vim.keymap.set('n', '<F8>', ':setlocal spell! spelllang=en_gb<CR>') -- spelling!
 
-vim.g.vimtex_complete_enabled = 1
--- vim.g.tex_flavor = 'latex'
--- vim.g.vimtex_view_general_viewer = 'evince'
-
-local use = require('packer').use
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'numToStr/Comment.nvim'
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use 'folke/which-key.nvim'
-
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
-    }
-  }
-  use 'hrsh7th/cmp-omni' -- works with vimtex!
-  use 'lervag/vimtex'
-end)
-require('Comment').setup()
-require('which-key').setup()
+require('rasmus.plugs')
+require('rasmus.treesitter')
+require('rasmus.colors')
+require('rasmus.lsp')
---
+++ lua/rasmus/plugs.lua
@@ -0,0 +1,40 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {'numToStr/Comment.nvim', config = true},
+  {'folke/which-key.nvim', config = true},
+  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+  {'VonHeikemen/lsp-zero.nvim',
+  dependencies = {
+    -- LSP Support
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lua',
+
+    -- Snippets
+    'L3MON4D3/LuaSnip',
+    'rafamadriz/friendly-snippets'
+  }
+},
+'hrsh7th/cmp-omni', -- works with vimtex!
+{'lervag/vimtex', config = function() vim.g.vimtex_complete_enabled = 1 end}
+})
