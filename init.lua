271f7254ebeeed52f33943a6170ca78b5e4a4e77
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2023-08-20 21:32:08 +0200

    reboot

--- init.lua
+++ init.lua
@@ -8,28 +8,85 @@ vim.opt.smartcase = true
 vim.opt.laststatus = 3
 vim.opt.mouse = 'a'
 vim.opt.signcolumn = 'yes'
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.list = true
-vim.opt.listchars = 'tab:» ,trail:-,extends:>,precedes:<,nbsp:+'
-vim.opt.expandtab = true
+vim.wo.number = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.o.backspace = 'indent,eol,start'
-vim.o.hlsearch = false
-vim.o.incsearch = true
-vim.o.completeopt = 'menuone,noselect'
-vim.o.breakindent = true
-vim.wo.number = true
-vim.cmd [[set completeopt-=preview]]
 
 vim.keymap.set('n', '<F8>', ':setlocal spell! spelllang=en_gb<CR>') -- spelling!
-vim.keymap.set('n', '<leader>l', ':Lines<CR>')
-vim.keymap.set('n', '<leader>f', ':Files<CR>')
-vim.keymap.set('n', '<leader>b', ':Buffers<CR>')
 
-require("rasmus.plugs")
-require("rasmus.colors")
-require("rasmus.treesitter")
-require("rasmus.completion")
-require("rasmus.term")
+-- Plugin manager 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+-- Plugins
+require("lazy").setup({
+	{'numToStr/Comment.nvim', config = true},
+  {'nvim-treesitter/nvim-treesitter', cmd = 'TSUpdate'},
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		dependencies = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip', dependencies = { "rafamadriz/friendly-snippets" }},
+			{'hrsh7th/cmp-path'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+		}
+	}
+})
+
+-- lsp-zero boilerplate
+local lsp = require('lsp-zero').preset({
+  manage_nvim_cmp = {
+    set_extra_mappings = true,
+		set_sources = 'recommended'
+  }
+})
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+lsp.setup_servers({'gopls'})
+lsp.setup()
+
+local cmp = require('cmp')
+cmp.setup({
+	mapping = {
+		['<CR>'] = cmp.mapping.confirm({select = false}),
+	},
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  }
+})
+
+-- Treesitter
+require'nvim-treesitter.configs'.setup {
+  ignore_install = { "gitignore" },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- Colors
+vim.cmd[[
+hi SignColumn guibg=NONE
+hi pmenu guibg=NONE
+]]
--- lua/rasmus/colors.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_set_hl(0, 'SignColumn', { bg='NONE' })
-vim.api.nvim_set_hl(0, 'pmenu', { fg='white', bg='#4d4d4d' })
-vim.api.nvim_set_hl(0, 'pmenusel', { fg='black', bg='#C7B446' })
-vim.api.nvim_set_hl(0, 'WinSeparator', { bg='NONE' })
-vim.api.nvim_set_hl(0, 'StatusLine', { bg='yellow', fg='black' })
