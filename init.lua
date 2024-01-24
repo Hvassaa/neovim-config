a1db6c934548ed941ad77302c2c2754522974286
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2024-01-24 20:04:28 +0100

    completion and java works :^)

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.cmd[[nnoremap <leader> <Nop>]]
+vim.cmd [[nnoremap <leader> <Nop>]]
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.opt.termguicolors = true
@@ -8,21 +8,13 @@ vim.opt.laststatus = 3
 vim.opt.mouse = 'a'
 vim.opt.signcolumn = 'yes'
 vim.wo.number = true
--- vim.opt.tabstop = 2
--- vim.opt.softtabstop = 2
--- vim.opt.shiftwidth = 2
 vim.keymap.set('n', '<F8>', ':setlocal spell! spelllang=en_gb<CR>') -- spelling!
-vim.cmd[[
+vim.cmd [[
 hi SignColumn guibg=NONE
-" hi pmenu guibg=NONE
-hi pmenu guibg=lightgray
-hi pmenu guifg=black
+hi pmenu guibg=NONE
+"hi pmenu guibg=black
+"hi pmenu guifg=white
 ]]
-
-vim.cmd[[
-let g:node_host_prog = '/home/rtb/.nvm/versions/node/v18.18.2/bin/node'
-]]
-
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -36,127 +28,135 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
 	{ "numToStr/Comment.nvim", config = true, lazy = false },
-	{ "neovim/nvim-lspconfig" },
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ 'nvim-telescope/telescope.nvim', tag = '0.1.4', dependencies = { 'nvim-lua/plenary.nvim' } },
-	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-	{ 'hrsh7th/nvim-cmp' },
-	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/cmp-buffer' },
-	{ "L3MON4D3/LuaSnip" },
-	{ "saadparwaiz1/cmp_luasnip" },
-	{ 'mfussenegger/nvim-jdtls' },
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/nvim-cmp",
+	"saadparwaiz1/cmp_luasnip",
+	"L3MON4D3/LuaSnip",
+	"mfussenegger/nvim-jdtls",
 })
 
--- TELESCOPE
-require('telescope').setup{
-	defaults = {
-		layout_strategy = "vertical",
-		file_ignore_patterns = {"node_modules", "build",},
-	},
-}
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>ft', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fs', builtin.treesitter, {})
+require("mason").setup({
+	ui = {
+		border = "rounded"
+	}
+})
+require("mason-lspconfig").setup()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require('lspconfig')
 
--- CMP
-local cmp = require('cmp')
-cmp.setup({
-	preselect = cmp.PreselectMode.None,
+lspconfig.lua_ls.setup {
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" } -- fixes unknown global "vim"
+			}
+		}
+	},
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+			client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+				Lua = {
+					runtime = {
+						version = 'LuaJIT'
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = vim.api.nvim_get_runtime_file("", true)
+					}
+				}
+			})
+
+			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+		end
+		return true
+	end
+}
+
+local luasnip = require 'luasnip'
+local cmp = require("cmp")
+cmp.setup {
 	snippet = {
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end,
-	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		--    documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+		['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+		-- C-b (back) C-f (forward) for snippet placeholder navigation.
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
 	}),
-	sources = cmp.config.sources({
+	sources = {
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' },
-	}, {
-		{ name = 'buffer' },
-	})
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+}
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set('n', '<space>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<space>f', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
+	end,
 })
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
---require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-	--  capabilities = capabilities
-	--}
-
-	-- LSP
-	require("mason").setup()
-	require("mason-lspconfig").setup()
-
-	-- Setup language servers.
-	local lspconfig = require('lspconfig')
-	lspconfig.tsserver.setup{ capabilities = capabilities }
-
-	-- Global mappings.
-	-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-	vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-	vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-	vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
-	-- Use LspAttach autocommand to only map the following keys
-	-- after the language server attaches to the current buffer
-	vim.api.nvim_create_autocmd('LspAttach', {
-		group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-		callback = function(ev)
-			-- Enable completion triggered by <c-x><c-o>
-			-- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-			-- Buffer local mappings.
-			-- See `:help vim.lsp.*` for documentation on any of the below functions
-			local opts = { buffer = ev.buf }
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-			vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-			vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-			vim.keymap.set('n', '<leader>wl', function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, opts)
-			vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-			vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-			vim.keymap.set('n', '<leader>lf', function()
-				vim.lsp.buf.format { async = true }
-			end, opts)
-		end,
-	})
-
-	-- TREESITTER
-	require'nvim-treesitter.configs'.setup {
-		-- A list of parser names, or "all" (the five listed parsers should always be installed)
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java", "javascript", "typescript" },
-		sync_install = false,
-		auto_install = true,
-
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-	}
-
