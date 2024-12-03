b6c61db402da0e2db3a160ceb99c3c4865e87ac8
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2024-12-03 22:07:21 +0100

    😶‍🌫️😶‍🌫️😶‍🌫️

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 vim.loader.enable()
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.opt.termguicolors = true
@@ -8,53 +7,98 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.mouse = 'a'
 vim.opt.signcolumn = "yes"
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.wo.number = true
-vim.cmd[[colorscheme koehler]]
+vim.cmd[[colorscheme retrobox]]
 
-local path_package = vim.fn.stdpath('data') .. '/site'
-local mini_path = path_package .. '/pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    'https://github.com/echasnovski/mini.nvim', mini_path
-  }
-  vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
-require("mini.extra").setup()
-require("mini.comment").setup()
-require("mini.completion").setup()
-require("mini.pick").setup()
-require("mini.deps").setup()
-
-MiniDeps.add({source = 'neovim/nvim-lspconfig'})
-
-require("lspconfig").lua_ls.setup({
-  cmd = {"/home/rasmus/Downloads/lua-language-server-3.9.0-linux-x64/bin/lua-language-server"},
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          'vim',
-          'require'
-        },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{"neovim/nvim-lspconfig"},
+	{
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate",
+		config = function () 
+			require("nvim-treesitter.configs").setup({
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false
+				}
+			})
+		end
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/cmp-nvim-lsp-signature-help"
+		},
+		config = function () 
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "vsnip" },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "path" }
+				}),
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-i>"] = cmp.mapping.open_docs(),
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4)
+				})
+			})
+		end
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" }
+	},
 })
+
+vim.keymap.set('n', '<F6>', vim.lsp.buf.rename)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<leader>r', vim.lsp.buf.references)
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
+-- vim.keymap.set('n', '<leader>fs', require('telescope.builtin').lsp_workspace_symbols)
+vim.keymap.set('n', '<leader>fs', require('telescope.builtin').treesitter)
+
+local lsp = require("lspconfig")
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+lsp.gopls.setup({
+	-- cmd = {'gopls'},
+	-- capabilities = capabilities
+})
+
+lsp.rust_analyzer.setup({})
